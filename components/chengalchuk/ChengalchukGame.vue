60ac2184c4f9e788992b962523cf658946417ad5
muhammadjon8 <umuhammadjon22@gmail.com>
2024-07-24 11:49:10 +0500

    #

---
+++ components/chengalchuk/ChengalchukGame.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="container flex justify-center flex-col items-center gap-5">
+    <div class="flex items-center mb-9">
+      <img src="/rock-paper-scissors-icon.png" alt="" class="size-32" />
+      <h2 class="text-3xl text-yellow-500">Rock Paper Scissors Game</h2>
+    </div>
+    <p>Choose your option</p>
+    <div class="flex gap-5">
+      <button
+        v-for="(button, index) in buttons"
+        :key="index"
+        :class="[
+          'text-5xl border rounded-md p-5',
+          { 'bg-green-300': index === activeButtonIndex }
+        ]"
+        @click="handleClick(index)"
+      >
+        {{ button.label }}
+      </button>
+    </div>
+    <button
+      @click="startFight"
+      class="px-7 py-3 bg-yellow-300 rounded-xl hover:text-white hover:bg-yellow-500"
+    >
+      Fight
+    </button>
+    <div class="flex gap-5 items-center">
+      <div>
+        <p class="text-center">You chose</p>
+        <p class="text-center text-7xl" v-if="activeButtonIndex !== null">
+          {{ userChoice }}
+        </p>
+      </div>
+      <div v-if="isFightStarted" >
+        <p class="text-center">Mine is</p>
+        <p class="text-center text-5xl" v-if="isFightStarted">
+          <img :src="myChoice.img" :alt="myChoice.label" class="size-60" />
+        </p>
+      </div>
+    </div>
+    <div>
+      <p class="text-center text-3xl" v-if="result">
+        {{ result }}
+      </p>
+    </div>
+    <div class="flex gap-5 mt-5">
+      <div>
+        <p class="text-center">Wins: {{ state.wins }}</p>
+      </div>
+      <div>
+        <p class="text-center">Losses: {{ state.losses }}</p>
+      </div>
+      <div>
+        <p class="text-center">Ties: {{ state.ties }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, reactive, computed } from "vue";
+
+const buttons = ref([
+  { label: "✊" },
+  { label: "🖐️" },
+  { label: "✌️" },
+]);
+const images = ref([
+  { img: "/rock.png", label: "✊" },
+  { img: "/paper.png", label: "🖐️" },
+  { img: "/scissors.png", label: "✌️" },
+]);
+
+const activeButtonIndex = ref(null);
+const result = ref(null);
+const isFightStarted = ref(false);
+
+const myChoice = ref(null);
+const userChoice = computed(() => {
+  return activeButtonIndex.value !== null ? buttons.value[activeButtonIndex.value].label : null;
+});
+
+const state = reactive({
+  wins: 0,
+  losses: 0,
+  ties: 0,
+});
+
+const handleClick = (index) => {
+  activeButtonIndex.value = index;
+  isFightStarted.value = false;
+};
+
+const startFight = () => {
+  if (activeButtonIndex.value === null) return;
+  myChoice.value = null;
+  isFightStarted.value = true;
+  result.value = null;
+  setTimeout(() => {
+    myChoice.value = images.value[Math.floor(Math.random() * 3)];
+    calculateResult();
+    updateTotalResults();
+  }, 1000);
+};
+
+const calculateResult = () => {
+  if (activeButtonIndex.value === null) return;
+  const user = buttons.value[activeButtonIndex.value].label;
+  const computer = myChoice.value.label;
+  const outcomes = {
+    "✊": { "✊": "It's a tie!", "🖐️": "You lose!", "✌️": "You win!" },
+    "🖐️": { "✊": "You win!", "🖐️": "It's a tie!", "✌️": "You lose!" },
+    "✌️": { "✊": "You lose!", "🖐️": "You win!", "✌️": "It's a tie!" },
+  };
+  setTimeout(() => {
+    result.value = outcomes[user][computer];
+  }, 100);
+};
+
+const updateTotalResults = () => {
+  if (result.value.includes("You win!")) {
+    state.wins += 1;
+  } else if (result.value.includes("You lose!")) {
+    state.losses += 1;
+  } else if (result.value.includes("It's a tie!")) {
+    state.ties += 1;
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ components/memoryCard/CardGame.vue
@@ -0,0 +1,152 @@
+<script setup>
+const images = ref([
+  {
+    id: 1,
+    img: "/js.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 2,
+    img: "/ts.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 3,
+    img: "/nodejs.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 4,
+    img: "/nest.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 5,
+    img: "/js.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 6,
+    img: "/ts.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 7,
+    img: "/nodejs.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 8,
+    img: "/nest.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 9,
+    img: "/vue.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 10,
+    img: "/vue.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 11,
+    img: "/nuxt.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 12,
+    img: "/nuxt.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 13,
+    img: "/postgres.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 14,
+    img: "/postgres.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 15,
+    img: "/mongo.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 16,
+    img: "/mongo.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 17,
+    img: "/mysql.png",
+    isFound: false,
+    done: false,
+  },
+  {
+    id: 18,
+    img: "/mysql.png",
+    isFound: false,
+    done: false,
+  },
+]);
+
+const shuffleArray = (array) => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const shuffledData = ref([]);
+
+onMounted(() => {
+  shuffledData.value = shuffleArray([...images.value]);
+});
+function allDone(array) {
+  return array.every((item) => item.done === true);
+}
+</script>
+
+<template>
+  <div class="container mx-auto p-4 text-red-900">
+    <h1 class="text-center mb-4">This is a Memory Game for Flipping Cards</h1>
+    <div class="flex">
+      <div
+        class="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-6 w-1/2"
+      >
+        <LazyMemoryCardFlipCard
+          v-for="(item, index) in shuffledData"
+          :key="index"
+          :data="item"
+        />
+      </div>
+      <div class="w-1/2" v-if="allDone(images)">
+        <!-- <img src="/myImage.png" alt="" class="h-[600px] w-full"> -->
+        <h1>You won</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
