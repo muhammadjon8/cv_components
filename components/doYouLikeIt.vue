0f738eb9f33f52b669dd57bbd7992bf7fd03033c
muhammadjon8 <umuhammadjon22@gmail.com>
2024-07-25 11:51:38 +0500

    #

--- components/doYouLikeIt.vue
+++ components/doYouLikeIt.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed } from "vue";
 
 const text = ref("Do you like that?");
 
 const changeText = () => {
   text.value = "Thank you";
+  isAnimation.value = true;
 };
 
 const moveBtn = ref({});
@@ -13,14 +14,23 @@ const randomPosition = () => {
   const maxX = 400; // Maximum horizontal translation
   const maxY = 500; // Maximum vertical translation (adjusted for container height)
   moveBtn.value = {
-    transform: `translate(${Math.random() * maxX - maxX / 2}px, ${Math.random() * maxY - maxY / 2}px)`
+    transform: `translate(${Math.random() * maxX - maxX / 2}px, ${
+      Math.random() * maxY - maxY / 2
+    }px)`,
   };
 };
-
+const isAnimation = ref(false);
 </script>
 
 <template>
-  <div class="container py-9">
+  <div class="container py-9 relative">
+    <div class="absolute left-full w-[300px]" :class="isAnimation ? 'popup' : ''">
+      <div class="bg-white absolute left-32 top-16 rounded-lg py-2 px-3">
+        Thanks so much, appreciate it!
+      </div>
+      <div class="bg-white size-5 rounded-full left-72 top-32 absolute"></div>
+      <img src="/popup.png" alt="" />
+    </div>
     <h2 class="text-center">Do you like it</h2>
     <div
       class="flex bg-purple-600 rounded justify-center items-center p-8 flex-col h-[500px]"
@@ -50,4 +60,28 @@ const randomPosition = () => {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+@keyframes popup {
+  0% {
+    /* top: 0; */
+    left: 100%;
+  }
+  25% {
+    left: 0;
+  }
+  50% {
+    /* top: 100px; */
+    left: 0;
+  }
+  75% {
+    left: 0;
+  }
+  100% {
+    /* top: 0; */
+    left: 100%;
+  }
+}
+.popup {
+  animation: popup 3s normal;
+}
+</style>
